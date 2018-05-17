072bd2ddf3d12bef1b4ff7c890695d0de7dc0d97
AlejandroGM020 <alejandro.gm020@gmail.com>
2018-05-16 19:50:42 -0500

    Awebo Chucho

--- app/src/main/java/com/alejandrogm020/platzigram/view/ContainerActivity.java
+++ app/src/main/java/com/alejandrogm020/platzigram/view/ContainerActivity.java
@@ -1,19 +1,61 @@
 package com.alejandrogm020.platzigram.view;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BaseTransientBottomBar;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.alejandrogm020.platzigram.view.fragment.HomeFragment;
+import com.alejandrogm020.platzigram.view.fragment.ProfileFragment;
+import com.alejandrogm020.platzigram.view.fragment.SearchFragment;
 import com.alejandrogm020.platzigram.R;
-//import com.roughike.bottombar.BottomBar;
 
 
 public class ContainerActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
 
-        //BottomBar bottomBar = (BottomBar) findViewById(R.id.bottombar);
+        final android.support.v4.app.Fragment homeFragment = new HomeFragment();
+        final android.support.v4.app.Fragment profileFragment = new ProfileFragment();
+        final android.support.v4.app.Fragment searchFragment = new SearchFragment();
+
+        if(savedInstanceState == null){
+            android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragmentContainer, homeFragment).commit();
+        }
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottombar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+
+                if(item.getItemId() == R.id.home_item){
+                    android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragmentContainer, homeFragment).commit();
+
+                }else if(item.getItemId() == R.id.search_item){
+                    android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragmentContainer, profileFragment).commit();
+
+                }else if(item.getItemId() == R.id.profile_item){
+                    android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragmentContainer, searchFragment).commit();
+                }
+                return false;
+            }
+        });
+
     }
 }
